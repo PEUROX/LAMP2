45ae13c202134d20d4c6b9fb694372b1b1fc41e9
haiyun xiao <haiyunx2160@gmail.com>
2019-03-03 17:51:02 -0500

    added pedro's version: added resetting and css. -haiyun

---
+++ lamp2_project2_pedro_version/config/database.php
@@ -0,0 +1,10 @@
+<?php
+// Guys Remind to chane for your user
+$db =  mysqli_connect('localhost','lamp3user','Test123!','pedro');
+
+if (mysqli_connect_errno()){
+	echo "<p style = 'color:red;'>Error: could not connect to the database<br/>
+      Please try again later</p>";
+}
+
+$success = 0;
